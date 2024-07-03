017cf06dc05e5dead9120b278ed34f458911a052
Sushil Dhakal <sushildhakal1@gmail.com>
2024-07-03 23:56:24 +1000

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -8,6 +9,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     //validation
     if(!name || !email || !password || !role){
         const error = createHttpError(400, "All fields are required");
+        return next(error);
+    }
+    //Database call
+    const user = await userModel.findOne({email});
+    if(user){
+        const error = createHttpError(400, "User already exists");
         return next(error);
     }
     //process
---
+++ src/user/userModel.ts
@@ -0,0 +1,97 @@
+import mongoose, {Schema} from "mongoose";
+
+const addressSchema = new Schema({
+    street: String,
+    city: String,
+    state: String,
+    zip_code: String,
+    country: String
+  });
+
+  const bookingSchema = new Schema({
+    booking_id: { type: Schema.Types.ObjectId, required: true },
+    package_id: { type: Schema.Types.ObjectId, required: true },
+    booking_date: { type: Date, required: true },
+    travel_date: { type: Date, required: true },
+    status: { type: String, required: true },
+    total_price: { type: Number, required: true },
+    payment_status: { type: String, required: true }
+  });
+  
+  const paymentMethodSchema = new Schema({
+    type: { type: String, required: true },
+    details: {
+      card_number: String,
+      expiry_date: String,
+      cardholder_name: String,
+      billing_address: addressSchema,
+      email: String
+    }
+  });
+  
+  const wishlistSchema = new Schema({
+    package_id: { type: Schema.Types.ObjectId, required: true },
+    added_date: { type: Date, required: true }
+  });
+  
+  const reviewSchema = new Schema({
+    review_id: { type: Schema.Types.ObjectId, required: true },
+    package_id: { type: Schema.Types.ObjectId, required: true },
+    review_date: { type: Date, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true }
+  });
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: { type: String, required: true, unique: true },
+    password_hash: { type: String, required: true },
+    roles: {
+        type: [String],
+        enum: ['user', 'admin', 'company', 'subscriber'],
+        default: ['user'],
+        validate: {
+          validator: function(v: string[]) {
+            return v.length > 0;
+          },
+          message: 'A user must have at least one role.'
+        }
+      },
+    phone: { type: String, required: true },
+    address: addressSchema,
+    preferences: {
+      language: { type: String, required: true },
+      currency: { type: String, required: true },
+      newsletter_subscribed: { type: Boolean, required: true }
+    },
+    bookings: [bookingSchema],
+    payment_methods: [paymentMethodSchema],
+    wishlist: [wishlistSchema],
+    reviews: [reviewSchema]
+},
+    {timestamps: true},
+);
+
+// Pre-save hook to enforce only one admin
+userSchema.pre('save', async function(next) {
+    const user = this;
+  
+    if (user.roles.includes('admin')) {
+      const adminCount = await mongoose.models.User.countDocuments({ roles: 'admin' });
+  
+      // If there is already an admin and the current user is not an admin, prevent saving
+      if (adminCount > 0 && !user.isModified('roles')) {
+        const currentAdmin = await mongoose.models.User.findOne({ roles: 'admin' });
+        if (currentAdmin && currentAdmin._id.toString() !== user._id.toString()) {
+          throw new Error('Only one admin is allowed.');
+        }
+      }
+    }
+  
+    next();
+  });
+
+export default mongoose.model("User", userSchema)
