c8ffb2daadcec900e35809944fcebe2a93c27f5e
Sushil Dhakal <sushildhakal1@gmail.com>
2024-07-03 15:33:41 +1000

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+//import cors from "cors";
+//import globalErrorHandler from "./middlewares/globalErrorHandler";
+// import userRouter from "./user/userRouter";
+// import bookRouter from "./book/bookRouter";
 
 const app = express();
 
 // Routes
-app.get("/", (req, res) => {
-    
+// HTTP GET, POST, PUT, PATCH, DELETE
+app.get("/", (req, res, next) => {
     res.json({message:"Hello World!"});
 });
 
+// Global error handler
+app.use(globalErrorHandler);
 
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+
+//global error handler
+
+const globalErrorHandler = (
+    err: HttpError, 
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => {
+
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json({
+        message: err.message,
+        errorStack: err.stack
+    })
+
+}
+export default globalErrorHandler;
