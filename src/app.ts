a6382aa38b9fa8a8d5b94ee88ec830bd50ba37bb
Sushil Dhakal <sushildhakal1@gmail.com>
2024-07-05 00:50:58 +1000

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,23 @@ import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import tourRouter from "./tours/tourRouter";
-//import cors from "cors";
-//import globalErrorHandler from "./middlewares/globalErrorHandler";
-// import userRouter from "./user/userRouter";
-// import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: config.frontendDomain,
+    })
+);
+
+
 app.use(express.json());
 // Routes
 // HTTP GET, POST, PUT, PATCH, DELETE
 app.get("/", (req, res, next) => {
-    res.json({message:"Hello World!"});
+    res.json({message:"Hello this is eTravel apis"});
 });
 
 
