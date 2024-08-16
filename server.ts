c7d86fcbec21d864d0dc235e8e0794d297573967
Sushil Dhakal <sushildhakal1@gmail.com>
2024-08-16 15:54:48 +1000

    change made for render

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 import connectDB from "./src/config/db";
 
 const startServer = async () => {
   // Connect database
   await connectDB();
 
-  const port = process.env.PORT || 4000;
+  const port =  Number(config.port) || 4000;
 
-  app.listen(port, () => {
+  app.listen(port, '0.0.0.0',() => {
     console.log(`Listening on port: ${port}`);
   });
 };
