b2f3371595a8c6c2d7d43a9ceb1bf2cbebaec73d
Sushil Dhakal <sushildhakal1@gmail.com>
2025-04-15 19:23:32 +1000

    server issue fixing

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,13 @@
 import { Breadcrumb } from '../middlewares/breadcrumbsMiddleware';
+import { Multer } from 'multer';
 
 
 declare global {
   namespace Express {
     interface Request {
       breadcrumbs?: Breadcrumb[];
+      file?: Multer.File;
+      files?: Record<string, Multer.File[]>;
     }
   }
 }
